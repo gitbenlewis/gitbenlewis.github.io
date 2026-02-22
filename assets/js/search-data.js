2ce4aeefa6d53b097d2b755995be4d2fe991cbe1
gitbenlewis <105738044+gitbenlewis@users.noreply.github.com>
2026-02-22 02:33:14 +0000

    Deploying to gh-pages from @ gitbenlewis/gitbenlewis.github.io@309394ffe98c4508bc4ac13e46bb3395d9bd07f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
